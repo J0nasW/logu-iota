74a7b19608958150122e5d75cadb0c69fb41606f
Jonas W <31929262+J0nasW@users.noreply.github.com>
2020-04-27 01:17:18 +0200

    updates

---
+++ prototype/dht11.js
@@ -0,0 +1,33 @@
+/*
+A Script by Jonas Wilinski working @ LogU TUHH.
+
+Run the following first:
+
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.46.tar.gz
+tar zxvf bcm2835-1.46.tar.gz
+cd bcm2835-1.46
+./configure
+make
+sudo make check
+sudo make install
+sudo npm install -g node-dht-sensor
+
+*/
+
+var sensorLib = require('node-dht-sensor');
+
+// Setup sensor, exit if failed
+var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
+var sensorPin  = 4;  // The GPIO pin number for sensor signal
+if (!sensorLib.initialize(sensorType, sensorPin)) {
+    console.warn('Failed to initialize sensor');
+    process.exit(1);
+}
+
+// Automatically update sensor value every 2 seconds
+setInterval(function() {
+    var readout = sensorLib.read();
+    
+    console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
+    console.log('Humidity:   ', readout.humidity.toFixed(1)    + '%');
+}, 2000);
