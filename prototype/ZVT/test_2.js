2dfda886e34531b4f36b6457e419b4f1122c43ce
J0nasW <jonas@boostnow.de>
2020-07-10 15:16:10 +0200

    Created the REACT App

--- prototype/ZVT/test_2.js
+++ prototype/ZVT/test_2.js
@@ -15,21 +15,16 @@ const address =
 iota
   .findTransactionObjects({ addresses: [address] })
   .then(response => {
-    const msg = response
+      msg = response
       .sort(function (a, b) { return b.timestamp - a.timestamp; })
-      .map(tx => tx.signatureMessageFragment)
-      .join('')
-    
-    msg_slice = msg.slice(0,2186);
-    console.log(msg_slice)
-    const data = Converter.trytesToAscii(msg_slice)
-    
-    console.log('Encoded message:')
-    console.log(data)
 
-    //Parse JSON
-    var payload = JSON.parse(data);
-    console.log(payload.Temperature)
+    console.log('Encoded message:')
+    console.log(msg)
+
+    //Convert trytes to plan text
+    //const data = Converter.trytesToAscii(msg)
+    //console.log('Decoded message:')
+    //console.log(data)
   })
   .catch(err => {
     console.error(err)
